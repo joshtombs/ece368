20bbc1e3b612e4032a2c25d4dda4d3f279b40883
Josh <joshtombs@gmail.com>
2015-03-04 16:43:16 -0500

    Create decode data path

---
+++ Project/RISC/decode.vhd
@@ -0,0 +1,34 @@
+---------------------------------------------------
+-- School:     University of Massachusetts Dartmouth
+-- Department: Computer and Electrical Engineering
+-- Engineer:   Josh Tombs
+-- 
+-- Create Date:    SPRING 2015
+-- Module Name:    Decode Block
+-- Project Name:   UMD_RISC16
+-- Target Devices: Spartan-3E
+-- Tool versions:  Xilinx ISE 14.7
+-- Description:    Decode portion of Datapath
+---------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decode is
+    Port( INST_IN : in  STD_LOGIC_VECTOR (15 downto 0);
+          DATA_OUT: out STD_LOGIC_VECTOR (43 downto 0));
+end decode;
+
+architecture Behavioral of decode is
+    signal tmp : STD_LOGIC_VECTOR (43 downto 0) := (OTHERS => '0');
+begin
+    tmp(43 downto 40) <= INST_IN(15 downto 12);
+    tmp(39 downto 36) <= INST_IN(11 downto 8);
+    tmp(35 downto 32) <= INST_IN(7 downto 4);
+    tmp(31 downto 24) <= x"00";
+    tmp(23 downto 16) <= INST_IN(7 downto 0);
+    tmp(15 downto 4)  <= x"000";
+    tmp(3 downto 0)   <= INST_IN(7 downto 4);
+    
+    DATA_OUT <= tmp;
+end Behavioral;
+
---
+++ Project/RISC/decode_tb.vhd
@@ -0,0 +1,107 @@
+---------------------------------------------------
+-- School:     University of Massachusetts Dartmouth
+-- Department: Computer and Electrical Engineering
+-- Engineer:   Josh Tombs
+-- 
+-- Create Date:    SPRING 2015
+-- Module Name:    Decode Block Test Bench
+-- Project Name:   UMD_RISC16
+-- Target Devices: Spartan-3E
+-- Tool versions:  Xilinx ISE 14.7
+-- Description:    Tests for Decode portion of Datapath
+---------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY decode_tb IS
+END decode_tb;
+ 
+ARCHITECTURE behavior OF decode_tb IS 
+ 
+   -- Component Declaration for the Unit Under Test (UUT)
+   COMPONENT decode
+   PORT(
+        INST_IN :  IN   std_logic_vector(15 downto 0);
+        DATA_OUT : OUT  std_logic_vector(43 downto 0)
+       );
+   END COMPONENT;
+    
+
+   --Inputs
+   signal INST_IN : std_logic_vector(15 downto 0) := (others => '0');
+   signal CLK : std_logic;
+
+   --Outputs
+   signal DATA_OUT : std_logic_vector(43 downto 0);
+    
+    --Clock
+   constant CLK_period : time := 20 ns;
+ 
+BEGIN
+ 
+    -- Instantiate the Unit Under Test (UUT)
+   uut: decode PORT MAP (
+          INST_IN => INST_IN,
+          DATA_OUT => DATA_OUT
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+        CLK <= '0';
+        wait for CLK_period/2;
+        CLK <= '1';
+        wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin        
+      -- hold reset state for 100 ns.
+      wait for 100 ns;    
+
+      wait for CLK_period*10;
+      REPORT "Beginning Decode Unit Tests." SEVERITY NOTE;
+      -- Instruction 1
+      INST_IN <= x"0FC0";
+      wait for CLK_period;
+      ASSERT (DATA_OUT(43 downto 40) = x"0")
+        REPORT "Opcode not correct."
+        SEVERITY WARNING;
+      ASSERT (DATA_OUT(39 downto 36) = x"F")
+        REPORT "Reg A Address not correct."
+        SEVERITY WARNING;
+      ASSERT (DATA_OUT(35 downto 32) = x"C")
+        REPORT "Reg B Address not correct."
+        SEVERITY WARNING;
+      ASSERT (DATA_OUT(31 downto 16) = x"00C0")
+        REPORT "Immediate value not correct."
+        SEVERITY WARNING;
+      ASSERT (DATA_OUT(15 downto 0) = x"000C")
+        REPORT "Shift Immediate value not correct."
+        SEVERITY WARNING;
+      
+      -- Instruction 2
+      INST_IN <= x"4321";
+      wait for CLK_period;
+      ASSERT (DATA_OUT(43 downto 40) = x"4")
+          REPORT "Opcode not correct."
+          SEVERITY WARNING;
+      ASSERT (DATA_OUT(39 downto 36) = x"3")
+          REPORT "Reg A Address not correct."
+          SEVERITY WARNING;
+      ASSERT (DATA_OUT(35 downto 32) = x"2")
+          REPORT "Reg B Address not correct."
+          SEVERITY WARNING;
+      ASSERT (DATA_OUT(31 downto 16) = x"0021")
+          REPORT "Immediate value not correct."
+          SEVERITY WARNING;
+      ASSERT (DATA_OUT(15 downto 0) = x"0002")
+          REPORT "Shift Immediate value not correct."
+          SEVERITY WARNING;
+            
+      wait;
+   end process;
+
+END;
